15962a8da40f9fd7c4ecb6e67012d1a7e6826cbc
EC2 Default User <ec2-user@ip-172-31-2-132.ap-northeast-1.compute.internal>
2022-09-07 13:13:58 +0000

    [Finish] 4-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,17 @@
 class BooksController < ApplicationController
   def new
     @books = Book.all
-    @new = Book.new
+    @book = Book.new
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to "/books"
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to "/books/#{@book.id}"
+    else
+      render :new
+    end
   end
 
   def show
@@ -16,6 +20,25 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find_by(id: params[:id])
+  end
+
+  def destroy
+    book_delete = Book.find_by(id: params[:id])
+    book_delete.destroy
+    redirect_to "/books/new"
+    flash[:notice] = "Book was successfully destroyed."
+  end
+
+  def update
+    @book = Book.find_by(id: params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      flash[:alarm] = "Book was successfully updated."
+      render :edit
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   get '/' => 'homes#top'
-  get 'books' => 'books#new'
-  post 'books' => 'books#create'
-  post 'books/edit'
-  get 'books/:id' => 'books#show', as: "show_link"
+  resources :books
 end
